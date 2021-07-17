3914ee9d39d70222d358536f425c5f201dbe788b
vincent <vincentuong@yahoo.com>
2021-07-17 15:10:05 -0500

    commit tree 9ac70da705bff8d74f02040f82660899cbb0a8c7

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,22 +1,50 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
-  
-  def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title && course.title != ""
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
-    end
+# doc.css(".post").first.css("h2").text 
+# doc.css(".post").first.css(".date").text
+# doc.css(".post").first.css("p").text 
+
+#The page itself:
+#doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+#The collection of course offerings:
+#doc.css(".post")
+#The title of an individual course offering:
+#doc.css(".post").first.css("h2").text
+#The schedule of an individual course offering:
+#doc.css(".post").first.css(".date").text
+#The description of an individual course offering:
+#doc.css(".post").first.css("p").text
+
+  def get_page
+      doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+
+      #doc.css(".post").each do |post|
+        #course = Course.new
+        #course.title = post.css("h2").text
+        #course.schedule = post.css(".date").text
+        #course.description = post.css("p").text
+      #end
+     
   end
   
-end
+  def get_courses
+    self.get_page.css(".post")
+  end
 
+  def make_courses
+    self.get_courses.each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
+  end 
+
+end
 
 
